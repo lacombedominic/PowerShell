72928cea78e2268f9673876b62e374de366d556c
Dominic LaCombe <139524753+lacombedominic@users.noreply.github.com>
2024-06-27 10:20:43 -0500

    hello_world

---
+++ hello_world/hello_world.ps1
@@ -0,0 +1,10 @@
+# Basic script. This will ask for user input and display "Hello + input".
+# Modification of the standard "Hello World" script.
+# Write-Host "Hello, World!"
+
+# Prompt the user for input
+$name = Read-Host "Please enter your name"
+
+# Print the greeting
+Write-Host "Hello, $name"
+
